708d6838259d5ed13ca6513a97b723b593751f98
Clément PÉAU <clement.peau@displayce.com>
2025-12-26 16:27:56 +0100

    Add UI shown keybind for mode change

--- pkg/gui/views.go
+++ pkg/gui/views.go
@@ -121,6 +121,7 @@ func (gui *Gui) createAllViews() error {
 	gui.Views.ModeTabs.Frame = true
 	gui.Views.ModeTabs.Tabs = []string{"Operation", "Maintenance"}
 	gui.Views.ModeTabs.TabIndex = int(gui.State.UIMode)
+	gui.Views.ModeTabs.TitlePrefix = "[Tab]"
 
 	gui.Views.Main.Wrap = gui.Config.UserConfig.Gui.WrapMainPanel
 	// when you run a docker container with the -it flags (interactive mode) it adds carriage returns for some reason. This is not docker's fault, it's an os-level default.
